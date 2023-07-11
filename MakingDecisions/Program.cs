d97b4883748d1f792269005316550d80fdc7c72a
lLija <ilija.trokovski@gmail.com>
2023-07-11 23:44:45 +0200

    Add project files.

---
+++ MakingDecisions/Program.cs
@@ -0,0 +1,60 @@
+﻿
+Console.WriteLine("Enter the current temperature");
+string temperature = Console.ReadLine();
+int numTemp;
+int number;
+bool userEnteredNum = int.TryParse(temperature, out number);
+
+if (userEnteredNum)
+{
+    numTemp = number;
+} else
+{
+   numTemp = 0;
+    Console.WriteLine("You entered the wrong value");
+}
+
+if (numTemp < 20)
+{
+    Console.WriteLine("Take the coat");
+}else if( numTemp == 20)
+{
+    Console.WriteLine("It's 20 degrees");
+}else
+{
+    Console.WriteLine("Cozy & warm");
+}
+
+// TryParse
+
+string numberAsString = "128";
+int parsedValue;
+
+bool success = int.TryParse(numberAsString, out parsedValue);
+
+if (success)
+{
+    Console.WriteLine("Parsing succesful - number is " + parsedValue);
+}else
+{
+    Console.WriteLine("Parsing failed");
+}
+
+string age = "18.75";
+float parsedFloatValue;
+
+bool parsedDoubleValue = float.TryParse(age, out parsedFloatValue);
+
+if (parsedDoubleValue)
+{
+    Console.WriteLine("Succesful");
+} else
+{
+    Console.WriteLine("Failed");
+}
+
+// Nested if Statement 
+
+
+
+Console.ReadLine();
